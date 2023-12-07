60e8f4fa31c8f3851f96543c513073c9e36231c9
almaskhan1286 <muhammadalmas1286@gmail.com>
2023-12-07 21:49:52 +0500

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+// import CircularCursor from "./components/CircularCursor";
+
+import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary App">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <AnimatedCursor
+        color="#fff"
+        innerSize={8}
+        outerSize={50}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0.1}
+        // outerStyle={{
+        //   mixBlendMode: 'exclusion',
+        // }}
+        outerStyle={{mixBlendMode:'exclusive'}}
+      
+
+        />
           <Navbar />
+          {/* <CircularCursor/> */}
           <Hero />
         </div>
         <About />
@@ -15,13 +43,13 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
